9520bbf18325735739d3f6ac2ded9031354ff0d7
Aman Agrahari <aman8cse@gmail.com>
2026-08-23 23:14:09 +0530

    Refactor authentication and user model logic for improved clarity

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    password: {type: String, required: true},
+    password: { type: String, required: true },
     avatar: { type: String, default: null },
-    zealId: {type: String},
-    rollNo: {type: String},
-    section: {type: String},
-    email: {type: String, unique: true, sparse: true}
+    zealId: { type: String },
+    rollNo: { type: String },
+    section: { type: String },
+    email: { type: String, unique: true, sparse: true },
   },
   { timestamps: true }
 );
